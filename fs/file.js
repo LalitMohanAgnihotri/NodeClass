b9a6ea65c52d3d47b89207c36ad3dab473b30f06
LalitMohanAgnihotri <lalitagnihotri62@gmail.com>
2026-02-05 15:15:29 +0530

    Paths

--- fs/file.js
+++ fs/file.js
@@ -1,5 +1,5 @@
-import add from './utils.js';
-import multi from './fuctions/function.js';
+import add from '../utils.js';
+import multi from '../fuctions/function.js';
 const result = add(5, 10);
 console.log(`The sum is: ${result}`);
 console.log(`The multiplication is: ${multi(5, 10)}`);
